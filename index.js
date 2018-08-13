77aedf9fed08bb60c8d30e5103b3dbddd1db875c
Jesé Rodríguez <jese200@gmail.com>
2018-08-13 07:13:02 -0400

    Implement HTTPS

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-require('dotenv').config();
+const path = require('path');
+const https = require('https');
 const express = require('express');
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const CERTS_PATH = process.env.CERT_PATH;
 
 const app = express();
 
@@ -11,7 +16,20 @@ app.get('/', (req, res) => {
   res.send('primetime-api');
 });
 
-app.listen(PORT, () => {
+// Support HTTPS
+let server = null;
+
+if (NODE_ENV === 'production') {
+  const options = {
+    key: path.resolve(CERTS_PATH, 'privkey.pem'),
+    cert: path.resolve(CERTS_PATH, 'cert.pem'),
+  };
+  server = https.createServer(options, app);
+} else {
+  server = app;
+}
+
+server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
